97aaaaa7b20776a365a052bd0eb12210c1a3238f
mikkugv <92909499+mikkugv@users.noreply.github.com>
2025-12-03 16:28:25 +0530

    Update half_adder.v

--- Day 1/Question 2/half_adder.v
+++ Day 1/Question 2/half_adder.v
@@ -5,3 +5,14 @@ endmodule
 
 
 
+
+/* OUTPUT
+meenakshi@meenakshi-Inspiron-3501:~/verilog/half_adder$ vvp half_adder.out
+VCD info: dumpfile half_adder.vcd opened for output.
+$time=0 |a=0 b=0, sum=0 ,cout=0
+$time=10 |a=0 b=1, sum=1 ,cout=0
+$time=20 |a=1 b=0, sum=1 ,cout=0
+$time=30 |a=1 b=1, sum=0 ,cout=1
+half_adder_tb.v:20: $finish called at 40 (1s)
+*/
+
