becc57aa4245ac8aa7e9f29dca404343ee7c10e2
mikkugv <92909499+mikkugv@users.noreply.github.com>
2025-12-03 16:25:28 +0530

    Add simulation output comments to or_gate.v
    Added simulation output comments for or_gate.

--- Day 1/ Question 1/or_gate.v
+++ Day 1/ Question 1/or_gate.v
@@ -5,3 +5,17 @@ module or_gate(
    );
 	assign y=a|b;
 endmodule
+
+
+
+
+
+/* OUTPUT 
+meenakshi@meenakshi-Inspiron-3501:~/verilog/or_gate$ vvp or_gate.out
+VCD info: dumpfile or_gate.vcd opened for output.
+time=0 | a=0 b=0 | y=0
+time=10 | a=0 b=1 | y=1
+time=20 | a=1 b=0 | y=1
+time=30 | a=1 b=1 | y=1
+or_gate_tb.v:19: $finish called at 40 (1s)
+*/
