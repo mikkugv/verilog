2fe37bb2c9222895044aedd6b8dcbbefcbbe9ae2
mikkugv <92909499+mikkugv@users.noreply.github.com>
2025-12-07 22:00:27 +0530

    Document simulation output in unsized_no.v
    Added simulation output comments for unsized_no.v.

--- Day 2/Question 1/unsized_no.v
+++ Day 2/Question 1/unsized_no.v
@@ -7,3 +7,10 @@ module unsized_no(
     assign bi_output  = bi_input;
     assign hex_output = hex_input;
 endmodule
+/*OUTPUT
+meenakshi@meenakshi-Inspiron-3501:~/verilog/unsized_no$ vvp unsized_no.out
+VCD info: dumpfile unsized_no.vcd opened for output.
+$time=0 | bi_input=00001001 | hex_input=0000101001110101 | bi_output=00001001 | hex_output=0000101001110101
+$time=10 | bi_input=00001010 | hex_input=0000101100010111 | bi_output=00001010 | hex_output=0000101100010111
+unsized_no_tb.v:23: $finish called at 20 (1s)
+*/
