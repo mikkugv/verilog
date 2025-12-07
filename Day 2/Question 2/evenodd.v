6dece4789a472f68bebf434591868bb2be444be8
mikkugv <92909499+mikkugv@users.noreply.github.com>
2025-12-07 22:42:18 +0530

    Update evenodd.v

--- Day 2/Question 2/evenodd.v
+++ Day 2/Question 2/evenodd.v
@@ -1,11 +1,10 @@
-module even_odd( 
-    input [3:0] a, 
-    output even,odd 
-    ); 
-assign even = (a[0] == 1'b0) ? 1'b1 : 1'b0; 
-assign odd = (a[0] == 1'b1) ? 1'b1 : 1'b0; 
-endmodule 
-
+module even_odd(
+    input  [3:0] a,
+    output even,odd
+);
+    assign even = ~a[0];
+    assign odd  = a[0];
+endmodule
 
 
 /* OUTPUT
