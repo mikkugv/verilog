db76b2b065e26c8e04b76d6ba2031486a8f44d09
mikkugv <92909499+mikkugv@users.noreply.github.com>
2025-12-07 21:58:43 +0530

    Update unsized_no.v

--- Day 2/Question 1/unsized_no.v
+++ Day 2/Question 1/unsized_no.v
@@ -1,1 +1,9 @@
-
+module unsized_no(
+    input  [7:0]  bi_input,
+    input  [15:0] hex_input,
+    output [7:0]  bi_output,
+    output [15:0] hex_output
+);
+    assign bi_output  = bi_input;
+    assign hex_output = hex_input;
+endmodule
