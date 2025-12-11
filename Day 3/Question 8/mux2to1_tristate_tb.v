83ea42209f7113232796a0fd7ebde208f62e676e
mikkugv <92909499+mikkugv@users.noreply.github.com>
2025-12-11 21:19:46 +0530

    Add files via upload

---
+++ Day 3/Question 8/mux2to1_tristate_tb.v
@@ -0,0 +1,23 @@
+module mux2to1_tristate_tb;
+reg a,b,s;
+wire y;
+//instantiate
+mux2to1_tristate dut(
+.a(a),
+.b(b),
+.s(s),
+.y(y)
+);
+initial begin 
+$monitor("$time=%0t|a=%b|b=%b|s=%b|y=%b",$time,a,b,s,y);
+$dumpfile("mux2to1_tristate.vcd");
+$dumpvars(0, mux2to1_tristate_tb);
+a=0; b=1; s=0; #10;
+a=1; b=0; s=0; #10;
+a=0; b=1; s=1; #10;
+a=1; b=0; s=1; #10;
+$finish;
+end
+endmodule
+
+
