69f3628840a86f3f1c08b04dfff3498ee79494bd
mikkugv <92909499+mikkugv@users.noreply.github.com>
2025-12-15 13:34:28 +0530

    Update logicgates_ifelse.v

--- Day 4 /Question 7/logicgates_ifelse.v
+++ Day 4 /Question 7/logicgates_ifelse.v
@@ -66,5 +66,21 @@ module logicgates_ifelse (
         else
             not_y = 1'bx;
     end
-
 endmodule
+
+
+
+/*output
+meenakshi@meenakshi-Inspiron-3501:~/verilog/logicgates_ifelse$ vvp logicgates_ifelse.out
+VCD info: dumpfile logicgates_ifelse.vcd opened for output.
+$time=0|a=0|b=0|and_y=0|or_y=0|nand_y=1|nor_y=1|xor_y=0|xnor_y=1|not_y=1
+$time=10|a=0|b=1|and_y=0|or_y=1|nand_y=1|nor_y=0|xor_y=1|xnor_y=0|not_y=1
+$time=20|a=1|b=0|and_y=0|or_y=1|nand_y=1|nor_y=0|xor_y=1|xnor_y=0|not_y=0
+$time=30|a=1|b=1|and_y=1|or_y=1|nand_y=0|nor_y=0|xor_y=0|xnor_y=1|not_y=0
+$time=40|a=x|b=1|and_y=x|or_y=1|nand_y=x|nor_y=0|xor_y=x|xnor_y=x|not_y=x
+$time=50|a=1|b=x|and_y=x|or_y=1|nand_y=x|nor_y=0|xor_y=x|xnor_y=x|not_y=0
+$time=60|a=z|b=0|and_y=0|or_y=x|nand_y=1|nor_y=x|xor_y=x|xnor_y=x|not_y=x
+$time=70|a=1|b=z|and_y=x|or_y=1|nand_y=x|nor_y=0|xor_y=x|xnor_y=x|not_y=0
+$time=80|a=x|b=z|and_y=x|or_y=x|nand_y=x|nor_y=x|xor_y=x|xnor_y=x|not_y=x
+logicgates_ifelse_tb.v:35: $finish called at 90 (1s)
+*/
