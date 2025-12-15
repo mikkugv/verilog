7e8663971d1b736cd12935f33287bedd96b30629
mikkugv <92909499+mikkugv@users.noreply.github.com>
2025-12-15 15:17:12 +0530

    Update twoscomplement_signedmagnitude.v

--- Day 4 /Question 8/twoscomplement_signedmagnitude.v
+++ Day 4 /Question 8/twoscomplement_signedmagnitude.v
@@ -23,3 +23,17 @@ $dumpvars(0, twoscomplement_signmagnitude_tb);
         $monitor("time=%0t | a=%b | sign=%b | magnitude=%b",$time, a, sign, mag);
     end
 endmodule
+
+
+
+/*OUTPUT
+meenakshi@meenakshi-Inspiron-3501:~/verilog/twoscomplement_signedmagnitude$ vvp twoscomplement_signedmagnitude.out
+VCD info: dumpfile twoscomplement_signmagnitude.vcd opened for output.
+time=0 | a=00000101 | sign=0 | magnitude=0000101
+time=10 | a=00001010 | sign=0 | magnitude=0001010
+time=20 | a=11111011 | sign=1 | magnitude=0000101
+time=30 | a=11110110 | sign=1 | magnitude=0001010
+time=40 | a=10000001 | sign=1 | magnitude=1111111
+time=50 | a=01111111 | sign=0 | magnitude=1111111
+twoscomplement_signedmagnitude.v:20: $finish called at 60 (1s)
+*/
