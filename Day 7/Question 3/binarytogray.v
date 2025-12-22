93bce50dc5c809c48fb810c116499271b6c86748
mikkugv <92909499+mikkugv@users.noreply.github.com>
2025-12-22 10:03:59 +0530

    Update binarytogray.v

--- Day 7/Question 3/binarytogray.v
+++ Day 7/Question 3/binarytogray.v
@@ -4,3 +4,17 @@ module binarytogray (
 );
     assign gray = bin ^ (bin >> 1);
 endmodule
+
+
+/*output
+meenakshi@meenakshi-Inspiron-3501:~/verilog/binarytogray$ vvp binarytogray.out
+VCD info: dumpfile binarytogray.vcd opened for output.
+Time  Binary  Gray
+0 | 0000 | 0000
+10 | 0001 | 0001
+20 | 0010 | 0011
+30 | 0011 | 0010
+40 | 0100 | 0110
+50 | 1011 | 1110
+binarytogray_tb.v:19: $finish called at 60 (1s)
+*/
